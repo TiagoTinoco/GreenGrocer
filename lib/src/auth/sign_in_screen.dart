679c788cdf674ff209c1fa1fb5495be688028d86
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2023-02-06 14:59:09 -0300

    initial structure

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.lightGreen,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
